b8dc68eb6da46be2b703d50815e727d9302f8e97
Luis Ismael Lima <luisbsl@gmail.com>
2019-05-09 13:01:45 -0300

    spring boot boilerplate

---
+++ src/main/java/br/com/moviesilove/moviesilove/MoviesiloveApplication.java
@@ -0,0 +1,13 @@
+package br.com.moviesilove.moviesilove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesiloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesiloveApplication.class, args);
+	}
+
+}
